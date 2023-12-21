1d4a5c53065caa58b190250862c201e840071522
Milad Joodi <winbeta.com@gmail.com>
2023-12-21 21:45:20 +0330

    ♥

--- 01.Dashboard_Admin/src/Components/Footer.js
+++ 01.Dashboard_Admin/src/Components/Footer.js
@@ -4,11 +4,10 @@ function Footer() {
   return (
     <footer class="main-footer">
     {/* <!-- To the right --> */}
-    <div class="float-right d-sm-none d-md-block">
-      Anything you want
+    <div class=" d-sm-none d-md-block">
+      <strong className='text-center '>CopyLeft &copy; 2022 </strong>
     </div>
     {/* <!-- Default to the left --> */}
-
   </footer>
   )
 }
